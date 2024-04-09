1f32021b460c09b35e6b77bad0e0fa115de9d9c2
Noah Burton <nburton115@gmail.com>
2024-04-09 13:19:41 -0600

    Refactor BingoBoard rendering in BoardEditor component

--- gogobingo/src/BoardEditor.jsx
+++ gogobingo/src/BoardEditor.jsx
@@ -25,14 +25,14 @@ export default function BoardEditor() {
                 <div className="bg-white p-4 rounded-md w-11/12 h-5/6 m-auto">
                     <h2 className="text-2xl font-bold">Preview</h2>
                     <div className={getGridClassName()}>
-    {bingoBoard.map((task, index) => (
-        <div key={index} className="flex flex-col items-center justify-center border border-gray-300 overflow-auto">
-            <div className="w-16 h-16 flex items-center justify-center text-xs break-normal">
-                <p>{task}</p>
-            </div>
-        </div>
-    ))}
-</div>
+                    {bingoBoard.map((task, index) => (
+                        <div key={index} className="flex flex-col items-center justify-center border border-gray-300 overflow-auto">
+                            <div className="w-16 h-16 flex items-center justify-center text-xs break-normal">
+                                <p>{task}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
                     <button onClick={() => setIsModalOpen(false)}>Close</button>
                 </div>
             </div>
