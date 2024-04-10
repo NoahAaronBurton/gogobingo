d5b7e121e7f7798eb6f1840ee31022c18f6a0a1b
Noah Burton <nburton115@gmail.com>
2024-04-10 12:12:52 -0600

    server.js setup

---
+++ gogoserver/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // for parsing application/json
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
