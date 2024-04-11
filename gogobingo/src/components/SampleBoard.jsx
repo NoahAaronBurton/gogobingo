05f7f2c80dabe4ef2f887c7f7e65d87291868431
Noah Burton <nburton115@gmail.com>
2024-04-11 10:13:31 -0600

    Add SampleBoard component

---
+++ gogobingo/src/components/SampleBoard.jsx
@@ -0,0 +1,37 @@
+export default function SampleBoard() {
+
+    return (
+        <div className="container">
+            <div className="h-96 w-96 grid grid-rows-3 grid-flow-col">
+                <div>
+                    1
+                </div>
+                <div>
+                    2
+                </div>
+                <div>
+                    3
+                </div>
+                <div>
+                    4
+                </div>
+                <div>
+                    5
+                </div>
+                <div>
+                    6
+                </div>
+                <div>
+                    7
+                </div>
+                <div>
+                   8
+                </div>
+                <div>
+                   9
+                </div>
+
+            </div>
+        </div>
+    )
+}
